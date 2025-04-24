823314ea69fa012f7114c73eb4190af74848c5b1
enzo <en.contrerase@duocuc.cl>
2025-04-24 10:58:07 -0400

    Initial commit

---
+++ src/main/java/com/duoc/EduTech/EduTechApplication.java
@@ -0,0 +1,13 @@
+package com.duoc.EduTech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EduTechApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EduTechApplication.class, args);
+	}
+
+}
