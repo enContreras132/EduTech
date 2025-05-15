36865622b7d6c6520865da0f92ef20f6eb3a3998
Tvbaal <cristo.perezr@duocuc.cl>
2025-05-15 11:10:23 -0400

    BASE DE DATOS

--- src/main/java/com/duoc/EduTech/Model/GerenteCursos.java
+++ src/main/java/com/duoc/EduTech/Model/GerenteCursos.java
@@ -12,7 +12,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
-public class GerenteCursos  extends User{
+public class    GerenteCursos  extends User{
 
     @Id
     private int id;
