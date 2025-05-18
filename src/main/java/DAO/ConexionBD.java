263f9200abaf409b1d70458ec1bc150415423940
LucasS2005 <luca.salinasg@duocuc.cl>
2025-05-18 02:12:48 -0400

    avance

--- src/main/java/DAO/ConexionBD.java
+++ src/main/java/DAO/ConexionBD.java
@@ -1,18 +1,19 @@
 package DAO;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class ConexionBD {
-    private static final String URL = "jdbc:mysql://localhost:3306/testt";
+    private static final String URL = "jdbc:mysql://localhost:3306/testt?serverTimezone=UTC";
     private static final String USER = "root";
     private static final String PASSWORD = "";
 
-
     public static Connection conectar() {
         try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
             return DriverManager.getConnection(URL, USER, PASSWORD);
-        } catch (SQLException e) {
+        } catch (ClassNotFoundException | SQLException e) {
             e.printStackTrace();
             return null;
         }
---
+++ src/main/java/com/duoc/EduTech/Repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.duoc.EduTech.Repository;
+
+import com.duoc.EduTech.Model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository  // Opcional, pero se puede dejar
+public interface UserRepository extends JpaRepository<User, Integer> {
+    // Puedes agregar métodos personalizados aquí si quieres
+}
