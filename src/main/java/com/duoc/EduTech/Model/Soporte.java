19b41df3c4b9103a53e0e4df1d1d677735eac895
CETECOM <CETECOM@AVAAOV02LC0412.duoc.acad>
2025-05-19 09:00:24 -0400

    update soporte
    cambio de errores por el @

--- src/main/java/com/duoc/EduTech/Model/Soporte.java
+++ src/main/java/com/duoc/EduTech/Model/Soporte.java
@@ -1,6 +1,12 @@
 package com.duoc.EduTech.Model;
 
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
