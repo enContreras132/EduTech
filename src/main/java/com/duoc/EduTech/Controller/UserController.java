0ab9e76d2f21df39e6559bf088f741cc81056aca
CETECOM <CETECOM@AVAAOV02LC0412.duoc.acad>
2025-05-19 09:20:42 -0400

    delete all methods
    borrado de todos los metodos para comenzar de nuevo

--- src/main/java/com/duoc/EduTech/Controller/UserController.java
+++ src/main/java/com/duoc/EduTech/Controller/UserController.java
@@ -8,22 +8,5 @@ public class UserController {
     @Autowired
     UserRepository userRepository;
 
-    public String addUser (User user){
-        return userRepository.addUser(user);
-    }
 
-    public String deleteUser (int id){
-        return userRepository.removeUser(id);
-    }
-
-    public String getAllUsers(){
-        return userRepository.getAllUsers();
-    }
-    public String getUserById(int id){
-        return userRepository.getUser(id);
-    }
-
-    public String updateUser (int id, User user){
-        return userRepository.updateUser(id,user);
-    }
 }
