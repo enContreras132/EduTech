d8fb77de98b9ab394612dd85d321a73fccaf1667
Fabianxarii <fa.cuevast@duocuc.cl>
2025-05-21 19:39:17 -0400

    creacion branch gerente, clase y servicio

--- src/main/java/com/duoc/EduTech/Model/GerenteCursos.java
+++ src/main/java/com/duoc/EduTech/Model/GerenteCursos.java
@@ -1,6 +1,8 @@
 package com.duoc.EduTech.Model;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -9,10 +11,16 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+
 @Entity
 public class GerenteCursos {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
+    private String nombre;
+    private String correo;
+    private String contrasena;
+    private String departamento;
 
 }
