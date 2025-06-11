f1c53e8817953be02939bd14bca4831f21ef416d
cetecom_avaras <luca.salinasg@duocuc.cl>
2025-06-11 10:29:09 -0400

    Creacion de cursomodelassembler

---
+++ src/main/java/com/duoc/EduTech/Assemblers/CursoModelAssembler.java
@@ -0,0 +1,23 @@
+package com.duoc.EduTech.Assemblers;
+import com.duoc.EduTech.Controller.CursoController;
+import com.duoc.EduTech.Model.Curso;
+
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.server.RepresentationModelAssembler;
+import org.springframework.stereotype.Component;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*;
+
+@Component
+public class CursoModelAssembler implements RepresentationModelAssembler<Curso,EntityModel<Curso>> {
+
+    @Override
+    public EntityModel<Curso> ToModel(Curso curso){
+        return EntityModel.of(curso,
+                linkTo(methodOn(CursoController.class).GetCursoById(curso.getId())).withSelfRel(),
+                linkTo(methodOn(CursoController.class).updateCurso(curso.getId(),curso)).withRel("Put"),
+                linkTo(methodOn(CursoController.class).deleteCursoById(curso.getId())).withRel("DELETE"),
+                linkTo(methodOn(CursoController.class).getAllCursos()).withRel("Get")
+        );
+    }
+
+}
